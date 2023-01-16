cd57c16813f3978a9d1a4b8474c6fdc004690fe0
NicooleT <110849165+NicooleT@users.noreply.github.com>
2023-01-16 17:44:20 -0500

    Create Course.js

---
+++ part 2/Course info/components/Course.js
@@ -0,0 +1,38 @@
+ const Course = ({ course }) => {
+    return (
+      <div>
+        <Header name={course.name} />
+        <Parts parts={course.parts} />
+        <Total parts={course.parts} />
+      </div>
+    );
+  };
+  
+  const Header = ({ name }) => {
+    return (
+    <h1>{name}</h1>
+    )
+  };
+
+  const Parts = ({ parts }) => {
+    return (
+    <div>
+        {parts.map((part) => (<Part key={part.id} name={part.name} exercises={part.exercises} />))}
+    </div>
+    )
+  };
+
+  const Part = ({name, exercises}) => {
+    return(
+        <div>{name} {exercises} </div>
+    )
+  };
+
+  const Total = ({parts}) => {
+    const total = parts.reduce((accumulate, part) => {return accumulate + part.exercises;}, 0);
+    return (
+        <div>Total of {total} exercises</div>
+    )
+}
+  
+  export default Course;
